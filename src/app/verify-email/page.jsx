67319d5f6c9d9acc0f1eab96796ef515fdf86c9e
Aaditya Dev Verma <aditya.nomercy6@gmail.com>
2025-12-03 13:41:01 +0530

    Fix vercel bug

--- src/app/verify-email/page.jsx
+++ src/app/verify-email/page.jsx
@@ -4,8 +4,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
-export const dynamic = "force-dynamic";
-
+import { Suspense } from "react";
 function VerifyEmail() {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
@@ -32,22 +31,24 @@ function VerifyEmail() {
     fetchUserFromToken();
   }, []);
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      {isProcessing ? (
-        <h1 className="text-4xl font-extrabold md:text-5xl lg:text-6xl text-center leading-tight">
-          <span className="text-transparent bg-clip-text bg-gradient-to-br from-orange-500 from-35% via-pink-600 to-purple-600 ">
-            Parsing Token...
-          </span>
-        </h1>
-      ) : (
-        <button
-          className="button1 !p-2 bg-purple-700 !rounded-xl hover:bg-gray-800"
-          onClick={() => router.push("/login")}
-        >
-          Login
-        </button>
-      )}
-    </div>
+    <Suspense fallback={null}>
+      <div className="min-h-screen flex justify-center items-center">
+        {isProcessing ? (
+          <h1 className="text-4xl font-extrabold md:text-5xl lg:text-6xl text-center leading-tight">
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-orange-500 from-35% via-pink-600 to-purple-600 ">
+              Parsing Token...
+            </span>
+          </h1>
+        ) : (
+          <button
+            className="button1 !p-2 bg-purple-700 !rounded-xl hover:bg-gray-800"
+            onClick={() => router.push("/login")}
+          >
+            Login
+          </button>
+        )}
+      </div>
+    </Suspense>
   );
 }
 
